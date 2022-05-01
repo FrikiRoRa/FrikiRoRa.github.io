eba82e21103bb9b168bf1dd34a86dd62a4de9154
Ronan <entropysweeper@gmail.com>
2022-05-01 14:14:26 -0700

    I made the guessing game look better

--- game.js
+++ game.js
@@ -12,7 +12,7 @@
 */
 var numOfGuesses;
 var guessNum = 0;
-
+var specialNumber;
 var startGameButton = document.getElementById("startGameButton");
 startGameButton.addEventListener("click", function () {
   numOfGuesses = document.getElementById("numGuesses").value;
@@ -20,18 +20,21 @@ startGameButton.addEventListener("click", function () {
 
   // start the game
   document.getElementById("gameArea").style.display = "block";
+  guessNum = 0;
+  console.log("I'm thinking of a number between 0 and 100");
+  displayAlertInRandomColor("I'm thinking of a number between 0 and 100");
+  //We are enabling the guess button when the player restarts the game
+  guessButton.disabled = false;
   // show guess number
+
   document.getElementById("guessNumber").innerText = guessNum + 1;
+  var randomDecimalBetweenZeroAndOne = Math.random();
+  var randomDecimalBetweenZeroAndMax = randomDecimalBetweenZeroAndOne * 100;
+  var randomIntegerBetweenZeroAndMax = Math.floor(
+    randomDecimalBetweenZeroAndMax
+  );
+  specialNumber = randomIntegerBetweenZeroAndMax;
 });
-
-console.log("I'm thinking of a number between 0 and 100");
-displayAlertInRandomColor("I'm thinking of a number between 0 and 100");
-
-var randomDecimalBetweenZeroAndOne = Math.random();
-var randomDecimalBetweenZeroAndMax = randomDecimalBetweenZeroAndOne * 100;
-var randomIntegerBetweenZeroAndMax = Math.floor(randomDecimalBetweenZeroAndMax);
-
-var specialNumber = randomIntegerBetweenZeroAndMax;
 
 guessButton.addEventListener("click", function () {
   const guess = document.getElementById("guess").value;
@@ -61,15 +64,7 @@ guessButton.addEventListener("click", function () {
 });
 
 function displayAlertInRandomColor(msg) {
-  const colorOptions = [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "indigo",
-    "violet",
-  ];
+  const colorOptions = ["red", "orange", "green", "blue", "indigo", "violet"];
   var randomDecimalBetweenZeroAndOne = Math.random();
   var randomDecimalBetweenZeroAndMax =
     randomDecimalBetweenZeroAndOne * colorOptions.length;
